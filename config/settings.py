1c0cd5ef30bbdb523eb32cd44be64dc9110ef3f1
sobirovinc <100451010+sobirovinc@users.noreply.github.com>
2023-08-25 23:01:38 +0900

    decouple changed third time

--- config/settings.py
+++ config/settings.py
@@ -4,13 +4,13 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-config = Config(RepositoryEnv('.env'))
+config = Config(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='default_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
