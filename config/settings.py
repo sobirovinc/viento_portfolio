1ac1736da90e0c33c51dc6f8bc41f79a7bb15170
sobirovinc <100451010+sobirovinc@users.noreply.github.com>
2023-08-26 11:10:07 +0900

    config deleted

--- config/settings.py
+++ config/settings.py
@@ -3,8 +3,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-config = Config(BASE_DIR / '.env')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
